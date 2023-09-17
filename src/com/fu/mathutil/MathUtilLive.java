f84529072e3a0924d5e4e3740b5e1e984c9d761f
CapPT04 <phatasdasd0198@gmail.com>
2023-09-17 12:37:12 +0700

    lan thu hai dua code len

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -6,6 +6,10 @@ public class MathUtilLive {
         
     public static void main(String[] args) {
         System.out.println("5! = " + (MathUtil.getFactorial(5)));
+        //// thêm code 12:36 17/9/2023
+        System.out.println("5! = " + (MathUtil.getFactorial(5)));
+        System.out.println("5! = " + (MathUtil.getFactorial(5)));
+        System.out.println("5! = " + (MathUtil.getFactorial(5)));
                 
     }
     
