a13e8fda66cb30b6c0e77bf526442834efdfa7bf
CapPT04 <phatasdasd0198@gmail.com>
2023-09-17 12:12:04 +0700

    lan dau dua code len server

---
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -0,0 +1,12 @@
+
+package com.fu.mathutil;
+
+
+public class MathUtilLive {
+        
+    public static void main(String[] args) {
+        System.out.println("5! = " + (MathUtil.getFactorial(5)));
+                
+    }
+    
+}
